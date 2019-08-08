3b78386703457a35c6c44252bd58c4dcbf099ff3
Guillaume BOUDON <guillaumeboudon@gmail.com>
2019-08-08 08:25:06 +0200

    :sparkles: Init game board

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,7 @@ module Main exposing (main)
 
 import Browser
 import Html exposing (Html)
+import Html.Attributes as A
 
 
 
@@ -11,12 +12,35 @@ import Html exposing (Html)
 
 
 type alias Model =
-    { matrix : List (List Bool) }
+    { board : Board }
+
+
+type alias Board =
+    List (List CellState)
+
+
+type CellState
+    = Dead
+    | Alive
+
+
+settings =
+    { width = 20
+    , height = 20
+    , cellColorAlive = "#F00"
+    , cellColorDead = "#000"
+    , cellSide = "10px"
+    }
 
 
 init : Model
 init =
-    Model [ [] ]
+    { board = initialBoard }
+
+
+initialBoard : Board
+initialBoard =
+    List.repeat settings.height (List.repeat settings.width Dead)
 
 
 
@@ -25,15 +49,9 @@ init =
 -------------------------------------------------------------------------------
 
 
-type Msg
-    = NoOp
-
-
-update : Msg -> Model -> Model
-update msg model =
-    case msg of
-        NoOp ->
-            model
+update : msg -> Model -> Model
+update _ model =
+    model
 
 
 
@@ -42,9 +60,42 @@ update msg model =
 -------------------------------------------------------------------------------
 
 
-view : Model -> Html Msg
+view : Model -> Html msg
 view model =
-    Html.text "Conway's Game of Life"
+    Html.div []
+        [ Html.h1 [] [ Html.text "Conway's Game of Life" ]
+        , Html.table
+            [ A.style "border-spacing" "0" ]
+            (viewBoard model.board)
+        ]
+
+
+viewBoard : Board -> List (Html msg)
+viewBoard board =
+    let
+        rowToCells row =
+            Html.tr [] (List.map viewCell row)
+    in
+    List.map rowToCells board
+
+
+viewCell : CellState -> Html msg
+viewCell bool =
+    let
+        color =
+            case bool of
+                Dead ->
+                    settings.cellColorDead
+
+                _ ->
+                    settings.cellColorAlive
+    in
+    Html.td
+        [ A.style "background-color" color
+        , A.style "width" settings.cellSide
+        , A.style "height" settings.cellSide
+        ]
+        []
 
 
 
@@ -53,7 +104,7 @@ view model =
 -------------------------------------------------------------------------------
 
 
-main : Program () Model Msg
+main : Program () Model msg
 main =
     Browser.sandbox
         { init = init
